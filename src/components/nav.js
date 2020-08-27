3f40641ca7a85444d84d64ff4b31da0b929bf637
Phil Bennett <me@phil.is>
2020-08-27 14:37:02 +0200

    fix(nav): a small adjustment to the nav image

--- src/components/nav.js
+++ src/components/nav.js
@@ -27,14 +27,17 @@ const Nav = () => {
           <div className="flex items-center justify-between w-full md:w-auto">
             <a href="/" aria-label="Home">
               <Image
-                className="mr-4 mb-0 w-12"
-                fluid={data.icon.childImageSharp.fluid}
+                className="mr-4 mb-0"
+                fluid={data.icon.childImageSharp.fixed}
                 alt="Software is Easy, People are Hard"
+                style={{
+                  minWidth: 50
+                }}
               />
             </a>
             <div className="-mr-2 flex items-center md:hidden">
               <button type="button"
-                      className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 z-20 focus:text-gray-500 transition duration-150 ease-in-out"
+                      className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 z-20 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
                       id="main-menu" aria-label="Main menu" aria-haspopup="true"
               onClick={toggleMenu}>
                 <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -69,7 +72,7 @@ const Nav = () => {
       </span>
         </div>
       </nav>
-      <div className={isMenuOpen ? "opacity-100 scale-100 absolute top-0 inset-x-0 z-40 p-2 transition transform origin-top-right md:hidden": "opacity-0 scale-95 absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"}>
+      <div className={isMenuOpen ? "opacity-100 scale-100 absolute top-0 inset-x-0 z-40 p-2 transition transform origin-top-right md:hidden": "opacity-0 scale-95 absolute top-0 z-10 inset-x-0 p-2 transition transform origin-top-right md:hidden"}>
       <div className="rounded-lg shadow-md">
         <div className="rounded-lg bg-white shadow-xs overflow-hidden" role="menu"
              aria-orientation="vertical" aria-labelledby="main-menu">
@@ -77,9 +80,12 @@ const Nav = () => {
             <div>
               <a href="/" aria-label="Home">
                 <Image
-                  className="mr-4 mb-0 w-12"
+                  className="mr-4 mb-0"
                   fluid={data.icon.childImageSharp.fluid}
                   alt="Software is Easy, People are Hard"
+                  style={{
+                    minWidth: 50
+                  }}
                 />
               </a>
             </div>
