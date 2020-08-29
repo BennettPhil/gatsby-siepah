77bd9c82c1d5676e517d27aa5a838e8b8bf91cc4
Phil Bennett <me@phil.is>
2020-08-29 14:06:45 +0200

    fix(gatsby-node): moving feed generations into project

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,14 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
+const RSS = require(`rss`);
+const fs = require(`fs-extra`);
+
+const wrapper = promise => promise.then(result => {
+  if (result.errors) {
+    throw result.errors;
+  }
+  return result;
+});
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
@@ -62,3 +71,230 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+
+exports.onPostBuild = async ({
+  graphql
+}, siteMetadata) => {
+  const siteMeta = await graphql(`
+    query PostBuild {
+      site {
+        siteMetadata {
+          podcastOptions {
+            title
+            subtitle
+            description
+            summary
+            podcastType
+            siteUrl
+            imageUrl
+            feedUrl
+            language
+            copyright
+            authorName
+            ownerName
+            ownerEmail
+            managingEditor
+            webMaster
+            explicit
+            publicationDate
+            category1
+            subCategory1
+            category2
+            category3
+            timeToLive
+            outputPath
+          }
+          author
+          social {
+            twitter
+          }
+        }
+      }
+    }
+  `
+  )
+  const podcastOptions = siteMeta.data.site.siteMetadata.podcastOptions
+
+  const feedOptions = {
+    title: podcastOptions.title,
+    description: podcastOptions.description,
+    site_url: podcastOptions.siteUrl,
+    feed_url: podcastOptions.feedUrl,
+    image_url: podcastOptions.imageUrl,
+    language: podcastOptions.language,
+    copyright: podcastOptions.copyright,
+    docs: `https://help.apple.com/itc/podcasts_connect/#/itcb54353390`,
+    author: podcastOptions.authorName,
+    managingEditor: podcastOptions.managingEditor,
+    webMaster: podcastOptions.webMaster,
+    categories: [podcastOptions.category1, podcastOptions.category2, podcastOptions.category3],
+    pubDate: podcastOptions.publicationDate,
+    ttl: podcastOptions.timeToLive,
+    generator: `https://github.com/miller-productions/gatsby-plugin-podcast-feed-mdx`,
+    custom_namespaces: {
+      'itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd',
+      'googleplay': 'http://www.google.com/schemas/play-podcasts/1.0'
+    },
+    custom_elements: [{
+      'itunes:title': podcastOptions.title
+    }, {
+      'itunes:subtitle': podcastOptions.subtitle
+    }, {
+      'itunes:summary': podcastOptions.summary
+    }, {
+      'itunes:type': podcastOptions.podcastType
+    }, {
+      'itunes:explicit': podcastOptions.explicit
+    }, {
+      'itunes:author': podcastOptions.authorName
+    }, {
+      'itunes:owner': [{
+        'itunes:name': podcastOptions.ownerName
+      }, {
+        'itunes:email': podcastOptions.ownerEmail
+      }]
+    }, {
+      'itunes:image': {
+        _attr: {
+          href: podcastOptions.imageUrl
+        }
+      }
+    }, {
+      'itunes:category': [{
+        _attr: {
+          text: podcastOptions.category1
+        }
+      }]
+    }, {
+      'itunes:category': [{
+        _attr: {
+          text: podcastOptions.category2
+        }
+      }]
+    }, {
+      'itunes:category': [{
+        _attr: {
+          text: podcastOptions.category3
+        }
+      }]
+    }, {
+      'googleplay:author': podcastOptions.authorName
+    }, {
+      'googleplay:description': podcastOptions.summary
+    }, {
+      'googleplay:explicit': podcastOptions.explicit
+    }]
+  }; // create the rss feed
+
+  const feed = new RSS(feedOptions); // get the options for the episodes
+
+  const result = await wrapper(graphql(`
+      query {
+        podcastEpisodes: allMdx(
+          filter: {frontmatter: {
+            status: {eq: "published"},
+            type: {eq: "podcast-episode"}}}) {
+          edges {
+            node {
+              excerpt
+              id
+              frontmatter {
+                title
+                slug
+                guid
+                subtitle
+                url
+                season
+                episodeNumber
+                episodeType
+                publicationDate
+                author
+                size
+                duration
+                explicit
+                categories
+              }
+            }
+          }
+        }
+      }
+    `));
+  const episodes = result.data.podcastEpisodes.edges; // for each episode
+
+  episodes.forEach(edge => {
+    // gather the options
+    const {
+      excerpt
+    } = edge.node;
+    const {
+      title,
+      slug,
+      guid,
+      subtitle,
+      url,
+      season,
+      episodeNumber,
+      episodeType,
+      publicationDate,
+      author,
+      size,
+      duration,
+      explicit,
+      categories
+    } = edge.node.frontmatter; // add an episode item to the feed using the options
+
+    feed.item({
+      guid,
+      title,
+      date: publicationDate,
+      description: excerpt,
+      url: podcastOptions.siteUrl + slug,
+      categories,
+      author: author,
+      custom_elements: [{
+        'itunes:title': title
+      }, {
+        'itunes:subtitle': subtitle
+      }, season && {
+        'itunes:season': season
+      }, episodeNumber && {
+        'itunes:episode': episodeNumber
+      }, {
+        'itunes:duration': duration
+      }, {
+        'itunes:episodeType': episodeType
+      }, {
+        'itunes:explicit': explicit
+      }, {
+        'itunes:summary': excerpt
+      }, {
+        'itunes:author': author
+      }, {
+        'itunes:image': {
+          _attr: {
+            href: feedOptions.image_url
+          }
+        }
+      }, {
+        'googleplay:description': excerpt
+      }, {
+        'googleplay:explicit': explicit
+      }],
+      enclosure: {
+        url,
+        size,
+        type: 'audio/mpeg'
+      }
+    });
+  }); // write the rss out to a file
+
+  const publicPath = `./public`;
+  const outputPath = path.join(publicPath, podcastOptions.outputPath);
+  const outputDir = path.dirname(outputPath);
+
+  if (!(await fs.exists(outputDir))) {
+    await fs.mkdirp(outputDir);
+  }
+
+  await fs.writeFile(outputPath, feed.xml());
+};
